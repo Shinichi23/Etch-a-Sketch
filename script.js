74992a54d113760ca7934eaedd1527bb4265c63c
Taha <taha-chaoui@hotmail.com>
2022-01-16 23:18:53 +0100

    85%

--- script.js
+++ script.js
@@ -1,42 +1,57 @@
 const container = document.querySelector("#container");
 const reset = document.createElement("button");
 reset.textContent = "RESET";
-const body = document.querySelector("body");
+const clear = document.createElement("button");
+clear.textContent = "CLEAR";
+const buttons = document.querySelector("#buttons");
 
-body.appendChild(reset); //
+buttons.appendChild(clear);
+buttons.appendChild(reset); //
 
-function grid(grid) {
-  for (let i = 0; i < 16 ** 2; i++) {
-    grid = document.createElement("div");
+/*function gridCreate() {
+  const grid = document.createElement("div");
+  grid.classList.add("grid");
+  container.appendChild(grid);
+}*/
+
+// gridCreate();
+function box(num) {
+  for (let i = 0; i < num ** 2; i++) {
+    let grid = document.createElement("div");
     grid.classList.add("grid");
     container.appendChild(grid);
   }
+  container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${num}, 1fr)`;
   const grids = document.querySelectorAll(".grid");
-
   grids.forEach((grid) => {
     grid.addEventListener("mouseover", function (e) {
-      e.target.style.backgroundColor = "red";
+      const Color1 = Math.floor(Math.random() * 256);
+      const Color2 = Math.floor(Math.random() * 256);
+      const Color3 = Math.floor(Math.random() * 256);
+      e.target.style.backgroundColor = `rgb(${Color1}, ${Color2}, ${Color3})`;
     });
   });
 }
 
-function resetSketch() {
-  const num = prompt();
-  return (
-    (document.getElementById(
-      "container"
-    ).style.gridTemplateColumns = `repeat(${num}, 30px)`),
-    (document.getElementById(
-      "container"
-    ).style.gridTemplateRows = `repeat(${num}, 30px)`)
-  );
+box(16);
+
+function deleteGrid() {
+  let boxes = document.querySelectorAll(".grid");
+  boxes.forEach((box) => {
+    container.removeChild(box);
+  });
 }
 
-reset.addEventListener("click", (e) => {
-  resetSketch(e);
+const buttonReset = reset.addEventListener("click", () => {
+  deleteGrid();
+  let num = prompt();
+  if (num > 99 || num < 2) {
+    (num = prompt()), box(num);
+  } else return box(num);
 });
 
-/*
- */
-
-grid();
+const buttonClear = clear.addEventListener("click", () => {
+  deleteGrid();
+  box(16);
+});
